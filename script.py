a24e5699407b92812abbbe9c907d6b8046fa4f63
BornRiot <marvindacosta@hotmail.com>
2021-02-28 22:03:50 -0500

    added files

--- script.py
+++ script.py
@@ -1,18 +1,15 @@
-""" Docstring"""
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    """Docstring"""
     return render_template("home.html")
     # return "Site Homepage"
 
 
 @app.route("/about/")
 def about():
-    """Docstring"""
     return render_template("about.html")
 
 
